a7441c5f66dee62c7b08f3616a33c1efa494a3e0
Mohammad Moradi <mrmohammad.moradi@yahoo.com>
2022-04-29 18:48:22 +0430

    install and import bootstrap and icon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='titre text-danger'>Hello</h1>
     </div>
   );
 }

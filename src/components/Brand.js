4e7bd2cf433d2f9163ecb84e3189bcce64b26e13
Mohammad Moradi <mrmohammad.moradi@yahoo.com>
2022-05-23 00:52:08 +0430

    Build Brand Component

---
+++ src/components/Brand.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import ease from '../assets/Image/EASE.png'
+
+
+function Brand() {
+    return (
+        <div className='BrandArshivePage m-3'>
+            <img className='mainArchivEase w-100 h-100' src={ease} alt='EASE'/>
+        </div>
+    )
+}
+
+export default Brand

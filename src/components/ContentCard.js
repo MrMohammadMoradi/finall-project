c2574294ea957c80d089bc619d08eb68e7aaaaef
Mohammad Moradi <mrmohammad.moradi@yahoo.com>
2022-05-23 20:05:45 +0430

    Build Content Card Component and set Style

---
+++ src/components/ContentCard.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function ContentCard() {
+    return (
+        <div className='ContentCard mb-3'>
+            <h3 className='CCTitle text-white text-end fw-bold me-3 my-4'>
+                این همون عنوان مطلب است
+            </h3>
+            <p className='CCDescription text-white text-end mt-3 me-3 ms-4'>
+                این یک متن آزمایشی برای تست جانمایی متون است و 
+                هیچ کار برد دیگری ندارد.
+                این یک متن آزمایشی برای تست جانمایی متون است و 
+                هیچ کار برد دیگری ندارد.
+                این یک متن آزمایشی برای تست جانمایی متون است و 
+                هیچ کار برد دیگری ندارد.
+
+            </p>
+            <button className='seeMore my-4 px-5 py-2 fw-bold border-0 rounded-pill'>
+                بیشتر بدانید 
+            </button>
+        </div>
+    )
+}
+
+export default ContentCard

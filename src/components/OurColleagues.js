4c5fbc57543624616d950b1b1752c70b12a5d7e1
Mohammad Moradi <mrmohammad.moradi@yahoo.com>
2022-06-04 18:08:08 +0430

    Build Our Colleagues Component

---
+++ src/components/OurColleagues.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function OurColleagues() {
+    return (
+        <div className='OurColleagues my-5'>
+            <div className='container my-5'>
+                <h2 className='my-3'>همکاران ما</h2>
+                <div className='SubTitle pb-5'>
+                    <h5 className=''>بهترین کادر آموزشی برای فرزندان شما</h5>
+                    <span className='Line mt-2'/>
+                </div>
+                <div className='PartnersSesion py-4'>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default OurColleagues

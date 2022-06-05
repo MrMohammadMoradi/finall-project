e85a58282167218e3f681cdf7c93d6a4b6fdd032
Mohammad Moradi <mrmohammad.moradi@yahoo.com>
2022-06-05 18:00:02 +0430

    Build Footer Component

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className='Footer text-white py-3'>
+            <div className='container'>
+                
+            </div>
+            
+        </div>
+    )
+}
+
+export default Footer

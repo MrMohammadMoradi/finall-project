d7d66b792994762267f93b3554a1c1fe2c7d8006
Mohammad Moradi <mrmohammad.moradi@yahoo.com>
2022-06-05 23:44:08 +0430

    Add ContactUs Component and Resources Component to Footer Component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import ContactUs from './ContactUs'
+import ReSources from './ReSources'
 
 function Footer() {
     return (
         <div className='Footer text-white py-3'>
-            <div className='container'>
-                
+            <div className='container d-flex'>
+                <ContactUs/>
+                <ReSources/>
             </div>
             
         </div>

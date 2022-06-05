de99d5c460dfbe9f75cb0540e3ad798289e5997e
Mohammad Moradi <mrmohammad.moradi@yahoo.com>
2022-06-06 00:00:37 +0430

    Add Tags to ShortcutLinks Component

--- src/components/ShortcutLinks.js
+++ src/components/ShortcutLinks.js
@@ -2,8 +2,20 @@ import React from 'react'
 
 function ShortcutLinks() {
     return (
-        <div className='ShortcutLinks'>
-
+        <div className='ShortcutLinks mt-5 col-lg-2 clo-sm-3 col-6 '>
+            <h5 className='mt-3 mb-4 fw-bold'> لینک های میانبر </h5>
+            <a className='my-3' href='#'>
+                <p> درباره ما </p>
+            </a>
+            <a className='my-3' href='#'> 
+                <p> خدمات ما </p>
+            </a>
+            <a className='my-3' href='#'> 
+                <p> دریافت نقل و قول </p>
+            </a>
+            <a className='my-3' href='#'> 
+                <p> محصولات </p>
+            </a>
         </div>
     )
 }

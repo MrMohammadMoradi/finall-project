147538a2418db1502de9df3e8e28dbb5e407a180
Mohammad Moradi <mrmohammad.moradi@yahoo.com>
2022-06-05 23:33:29 +0430

    Build ReSources Component

---
+++ src/components/ReSources.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function ReSources() {
+    return (
+        <div className='ReSources mt-5 col-lg-2 clo-sm-3 col-6 '>
+            <h5 className='mt-3 mb-4 fw-bold'> منابع مهم ما</h5>
+            <a className='my-3' href='#'>
+                <p> میدان فروش کالا </p>
+            </a>
+            <a className='my-3' href='#'> 
+                <p> مستندات </p>
+            </a>
+            <a className='my-3' href='#'> 
+                <p> مشتریان </p>
+            </a>
+            <a className='my-3' href='#'> 
+                <p>فرصت های شغلی </p>
+            </a>
+        </div>
+    )
+}
+
+export default ReSources

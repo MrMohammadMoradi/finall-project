5b9ccda48578cccfbd41690cee5da79cf863c7e7
Mohammad Moradi <mrmohammad.moradi@yahoo.com>
2022-06-06 14:15:02 +0430

    Add H5 and p to  NewsLetter Component

--- src/components/NewsLetter.js
+++ src/components/NewsLetter.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 function NewsLetter() {
     return (
-        <div className='NewsLetter' >
+        <div className='NewsLetter my-5 col-lg-4 clo-sm-6 col-12' >
+            <h5 className='fw-bold my-3'>
+                خبر نامه
+            </h5>
+            <p className='w-75'>
+                ثبت نام برای دریافت پیشنهادات ویژه ،‌
+                آموزش ، اخبار و بروزرسانی ها. 
+            </p>
 
         </div>
     )

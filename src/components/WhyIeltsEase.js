abff94b925e2df505e0a7ae81b34317e9f2346ed
Mohammad Moradi <mrmohammad.moradi@yahoo.com>
2022-06-09 19:28:08 +0430

    Build WhyIeltsEase.js

---
+++ src/components/WhyIeltsEase.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Title from './Title'
+
+function WhyIeltsEase() {
+    return (
+        <div className='WhyIeltsEase '>
+
+        </div>
+    )
+}
+
+export default WhyIeltsEase

17e26c31702ffc6e0bb4538ac0a8c5da5a43090c
Mohammad Moradi <mrmohammad.moradi@yahoo.com>
2022-06-09 17:49:16 +0430

    Add props for Title.js file

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function Title() {
+function Title({title, paragraph}) {
     return (
         <div className='Title'>
-            <h2 className='my-3'> </h2>
+            <h2 className='my-3'> {title} </h2>
             <div className='SubTitle pb-5'>
-                <h5 className=''>  </h5>
+                <h5 className=''> {paragraph} </h5>
                 <span className='Line mt-2'/>
             </div>
         </div>

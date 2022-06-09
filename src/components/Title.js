d2c107943065fe06d9d7586f300d283e9cbb3d3b
Mohammad Moradi <mrmohammad.moradi@yahoo.com>
2022-06-09 19:09:29 +0430

    change padding-bottom in Title.js file

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,7 +4,7 @@ function Title({title, paragraph}) {
     return (
         <div className='Title'>
             <h2 className='my-3'> {title} </h2>
-            <div className='SubTitle pb-5'>
+            <div className='SubTitle pb-3'>
                 <h5 className=''> {paragraph} </h5>
                 <span className='Line mt-2'/>
             </div>

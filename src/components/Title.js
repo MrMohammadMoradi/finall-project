67869128c27c859e2ace64084e5d5a8ce645a051
Mohammad Moradi <mrmohammad.moradi@yahoo.com>
2022-06-09 17:48:21 +0430

    Add h2 and p tag in Title.js file

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,11 @@ import React from 'react'
 function Title() {
     return (
         <div className='Title'>
-        
+            <h2 className='my-3'> </h2>
+            <div className='SubTitle pb-5'>
+                <h5 className=''>  </h5>
+                <span className='Line mt-2'/>
+            </div>
         </div>
     )
 }

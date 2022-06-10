7387aabfcbe9a7c28a22bc6a15c48a35b1b88c17
Mohammad Moradi <mrmohammad.moradi@yahoo.com>
2022-06-10 17:45:35 +0430

    Change Margin and padding in  WhyIeltsEase Component

--- src/components/WhyIeltsEase.js
+++ src/components/WhyIeltsEase.js
@@ -3,11 +3,15 @@ import Title from './Title'
 
 function WhyIeltsEase() {
     return (
-        <div className='WhyIeltsEase my-3'>
-            <div className='container'>
+        <div className='WhyIeltsEase'>
+            <div className='container text-white mt-3 py-4'>
                 <Title title={" آخرین دوره های آیلتس ایز"} 
                     paragraph={"چرا باید آیلتس ایز را انتخاب کنیم؟"}/>
                 
+                <div className='ReasonForWhy d-flex'>
+                    
+                </div>
+
             </div>
         </div>
     )

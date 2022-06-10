de510721690ae6fc51ee5f0b848bf6f83bc9f14e
Mohammad Moradi <mrmohammad.moradi@yahoo.com>
2022-06-10 13:16:35 +0430

    Add Title COmponent to  WhyIeltsEase.js

--- src/components/WhyIeltsEase.js
+++ src/components/WhyIeltsEase.js
@@ -3,8 +3,12 @@ import Title from './Title'
 
 function WhyIeltsEase() {
     return (
-        <div className='WhyIeltsEase '>
-
+        <div className='WhyIeltsEase my-3'>
+            <div className='container'>
+                <Title title={" آخرین دوره های آیلتس ایز"} 
+                    paragraph={"چرا باید آیلتس ایز را انتخاب کنیم؟"}/>
+                
+            </div>
         </div>
     )
 }

6fa35e08591f2b525d575aecbcd12af12e0473c0
Mohammad Moradi <mrmohammad.moradi@yahoo.com>
2022-06-11 23:36:08 +0430

    Change some tag and Attribute in Positive Point Carld and button tag in it

--- src/components/PositivePointCard.js
+++ src/components/PositivePointCard.js
@@ -2,15 +2,19 @@ import React from 'react'
 
 function PositivePointCard({image , title, description}) {
     return (
-        <div className='PositivePointCard d-flex'>
-            <img className='' src={image} alt=''/>
-            <div className=''>
+        <div className='PositivePointCard d-flex col-xl-4 col-md-6 col-12'>
+            <img className='w-25 h-25 pt-5' src={image} alt=''/>
+            <div className=' m-3'>
                 <h5 className='my-2'>
                     {title}
                 </h5>
-                <p> className=''
+                <p className=''> 
                     {description}
                 </p>
+                <button className=' text-white border-0'>
+                    بیشتر بدانید 
+                    <i class="bi bi-chevron-double-left"></i>
+                </button>
             </div>
 
         </div>

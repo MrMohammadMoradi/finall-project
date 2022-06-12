f16de6c25f9eee6fa05c949cd17784c13042a6b8
Mohammad Moradi <mrmohammad.moradi@yahoo.com>
2022-06-12 19:14:48 +0430

    Build Blog.js

---
+++ src/components/Blog.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Blog() {
+    return (
+        <div className='Blog'>
+            <div className='container'>
+                <div className='heder d-flex'>
+                
+                </div>
+                <div className='d-flex'>
+                    
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Blog

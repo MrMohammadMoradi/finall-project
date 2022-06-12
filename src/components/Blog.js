c6a4de6ae7f1ef4aeba5cfa26257697ee408857b
Mohammad Moradi <mrmohammad.moradi@yahoo.com>
2022-06-12 19:28:48 +0430

    Add Title.ja and button to Blog.js

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import Title from './Title'
 
 function Blog() {
     return (
         <div className='Blog'>
-            <div className='container'>
-                <div className='heder d-flex'>
-                
+            <div className='container py-3'>
+                <div className='text-white heder d-flex'>  
+                    <Title title={"چرا آیلتس ایز؟"}
+                        paragraph={"چرا باید آیلتس ایز را انتخاب کنیم؟"}/>
+                    <button className='fw-bold border-0 rounded-pill mb-3'>
+                        نمایش همه مطالب
+                    </button>
                 </div>
                 <div className='d-flex'>
                     

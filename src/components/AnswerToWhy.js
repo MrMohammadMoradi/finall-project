80cb10bbc24b3eae8f5fb647c08b16aa67973889
Mohammad Moradi <mrmohammad.moradi@yahoo.com>
2022-06-12 14:39:37 +0430

    Add Title.js to AnswerToWHy.js

--- src/components/AnswerToWhy.js
+++ src/components/AnswerToWhy.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Title from './Title'
 
 function AnswerToWhy() {
     return (
         <div className='AnswerToWhy'>
             <div className='container'>
-            
+                <Title title={"چرا آیلتس ایز؟"}
+                    paragraph={"چرا باید آیلتس ایز را انتخاب کنیم؟"}/>
             </div>
 
         </div>

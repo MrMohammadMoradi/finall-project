8983001178db8139df86ec3c44875f3fddfb2a5b
Mohammad Moradi <mrmohammad.moradi@yahoo.com>
2022-06-12 18:05:27 +0430

    Change Attribute and class and set props in AnswerCard.js

--- src/components/AnswerCard.js
+++ src/components/AnswerCard.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
-function AnswerCard() {
+function AnswerCard({image , number, title, description}) {
     return (
-        <div className='AnswerCard'>
-            <img className='' src='' alt=''/>
-            <h1 className='' > </h1>
-            <h5 className='' > </h5>
-            <p className='' > </p>
+        <div className='AnswerCard text-center mt-4 col-lg-3 col-sm-6 col-12'>
+            <div className='p-3'>
+                <img className='w-25 h-25' src={image} alt='imageAnswer'/>
+                <h1 className='number' > {number} </h1>
+                <h5 className='title' > {title} </h5>
+                <p className='' > {description} </p>
+            </div>
         </div>
     )
 }

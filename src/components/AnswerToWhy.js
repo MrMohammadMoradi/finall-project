67e288cdfc20981cc8acc29a603d91d80c917888
Mohammad Moradi <mrmohammad.moradi@yahoo.com>
2022-06-12 14:34:39 +0430

    Build AnswerToWHy.js

---
+++ src/components/AnswerToWhy.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function AnswerToWhy() {
+    return (
+        <div className='AnswerToWhy'>
+            <div className='container'>
+            
+            </div>
+
+        </div>
+    )
+}
+
+export default AnswerToWhy

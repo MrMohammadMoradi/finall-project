facbc49ad2d5420f8f56c393e62fb6b0d8f16623
Mohammad Moradi <mrmohammad.moradi@yahoo.com>
2022-06-13 14:29:00 +0430

    Build BlogCard.js and add several tags

---
+++ src/components/BlogCard.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function BlogCard() {
+    return (
+        <div className='BlogCard '>
+            <a className='' href='#'>
+                <img className='' src='' alt=''/>
+                <p className=''>
+
+                </p>
+                <h5 className=''>
+
+                </h5>
+                <div className=' d-flex'>
+                    <i class="bi bi-clock"></i>
+                    <p className=''>
+
+                    </p>
+                </div>
+            </a>
+
+        </div>
+    )
+}
+
+export default BlogCard

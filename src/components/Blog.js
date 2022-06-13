d7b184a3d07bb393212ab5730ccce7eed6970cd4
Mohammad Moradi <mrmohammad.moradi@yahoo.com>
2022-06-13 14:45:38 +0430

    Add data to Blog.js

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,7 +2,18 @@ import React from 'react'
 import Title from './Title'
 import BlogCard from './BlogCard'
 
+import img from '../assets/Image/Image.png'
+
 function Blog() {
+//image, category, title, date
+    const data = [
+        {id: 0, image: img, category: " آکادمی ها", title: "زن و شوهر از دانشجویان دانشگاه هپی فارغ التحصیل شد ", data: "۲۳ خرداد ۱۳۹۷"},
+        {id: 1, image: img, category: " دولت", title: "نکات مفید برای ایجاد یک بحث با کیفیت", data: "۲۳ خرداد ۱۳۹۷"},
+        {id: 2, image: img, category: " طراحی", title: "بازتاب فصل فجر", data: "۲۳ خرداد ۱۳۹۷"},
+        {id: 3, image: img, category: " طراحی", title: "شگفت انگیز است که دوستی هایی را که توسعه می یابند ببینید", data: "۲۳ خرداد ۱۳۹۷"}
+
+    ]
+
     return (
         <div className='Blog'>
             <div className='container py-3'>

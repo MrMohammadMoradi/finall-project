9a1121bf6d7e19c52e13984c3aab823fac07a382
Mohammad Moradi <mrmohammad.moradi@yahoo.com>
2022-06-16 00:13:22 +0430

    Add MainText and Sidebar component to  Archive Page

--- src/Pages/IeltsEaseArchivePage.js
+++ src/Pages/IeltsEaseArchivePage.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import Carosel from '../components/Carosel'
+import MainText from '../components/MainText'
+import Sidebar from '../components/SideBar.jsx'
 
 
 function IeltsEaseArchivePage() {
     return (
         <div className='IeltsEaseArchivePage'>
             <Carosel/>
+            <div className='container d-flex my-3'>
+                <MainText/>
+                <Sidebar/>
+            </div>
             
         </div>
     )

37cb6ed5902559f001b1697c198bd7cd94463b3a
Mohammad Moradi <mrmohammad.moradi@yahoo.com>
2022-06-15 23:51:50 +0430

    Build  Archive Page

---
+++ src/Pages/IeltsEaseArchivePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function IeltsEaseArchivePage() {
+    return (
+        <div className='IeltsEaseArchivePage'>
+            
+        </div>
+    )
+}
+
+export default IeltsEaseArchivePage
